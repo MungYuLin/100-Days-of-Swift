b1db8234af5d04528706243be6ff0c7d9b88572f
MungYuLin <linjiang.cx@gmail.com>
2016-05-08 13:50:34 +0800

    Swift iAD

---
+++ 002-Swift IAD/Swift IAD/Swift IAD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift IAD
+//
+//  Created by MungYu-Lin on 16/5/8.
+//  Copyright © 2016年 MY. All rights reserved.
+//
+
+import UIKit
+import iAd
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
