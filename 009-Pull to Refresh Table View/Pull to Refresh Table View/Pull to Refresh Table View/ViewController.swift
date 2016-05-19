f6c38c6c21b6cd667a89c494c5df08cbb88c36d0
MungYuLin <mungyulin@gmail.com>
2016-05-19 15:39:29 +0800

    Pull to Refresh Table View

--- 009-Pull to Refresh Table View/Pull to Refresh Table View/Pull to Refresh Table View/ViewController.swift
+++ 009-Pull to Refresh Table View/Pull to Refresh Table View/Pull to Refresh Table View/ViewController.swift
@@ -1,25 +1,26 @@
 //
 //  ViewController.swift
-//  Basic Table View
+//  Pull to Refresh Table View
 //
-//  Created by MungYu-Lin on 16/5/10.
+//  Created by MungYu-Lin on 16/5/19.
 //  Copyright © 2016年 MY. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
     var navigationBar: UINavigationBar!
     var tableView: UITableView!
     var leftButton: UIBarButtonItem!
     var rightButton: UIBarButtonItem!
-
+    var refreshControl: UIRefreshControl!
+    
     var datas: NSMutableArray = ["以父之名", "懦夫", "晴天", "三年二班", "东风破", "你听得到", "同一种调调", "她的睫毛", "爱情悬崖" ,"梯田", "双刀"]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         navigationBar = UINavigationBar(frame: CGRectMake(0, 20, 375, 44))
         let navigationItem = UINavigationItem()
         navigationItem.title = "枼惠美专辑列表"
@@ -29,39 +30,53 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         navigationItem.rightBarButtonItem = rightButton
         navigationBar.pushNavigationItem(navigationItem, animated: true)
         self.view.addSubview(navigationBar!)
-
+        
         tableView = UITableView(frame: CGRectMake(0, 65, 375, 600), style: .Plain)
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        tableView.allowsSelectionDuringEditing = true
         tableView.dataSource = self
-        tableView.delegate = self;
+        tableView.delegate = self
         self.view.addSubview(tableView)
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(ViewController.onPullToFresh), forControlEvents: UIControlEvents.ValueChanged)
+        refreshControl.attributedTitle = NSAttributedString(string: "釋放更新")
+        tableView.addSubview(refreshControl)
     }
-
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return datas.count
     }
-
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-        cell.textLabel?.text = datas.objectAtIndex(indexPath.row) as? String
+        cell.accessoryType = .DisclosureIndicator
+        cell.textLabel!.text = datas.objectAtIndex(indexPath.row) as? String
         return cell
     }
-
+    
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             datas.removeObjectAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Fade)
         }
     }
-
+    
     func tableView(tableView: UITableView, titleForDeleteConfirmationButtonForRowAtIndexPath indexPath: NSIndexPath) -> String? {
         return "删除"
     }
-
+    
     func addAction() {
+        datas.addObject("聽媽媽的話")
+        tableView.reloadData()
     }
-
+    
     func editAction() {
+        print("編輯~")
+    }
+    
+    func onPullToFresh() {
+        datas = ["以父之名", "懦夫", "晴天", "三年二班", "东风破", "你听得到", "同一种调调", "她的睫毛", "爱情悬崖" ,"梯田", "双刀"]
+        tableView.reloadData()
+        refreshControl.endRefreshing()
     }
 }
---
+++ 009-Pull to Refresh Table View/Pull to Refresh Table View/Pull to Refresh Table ViewTests/Pull_to_Refresh_Table_ViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pull_to_Refresh_Table_ViewTests.swift
+//  Pull to Refresh Table ViewTests
+//
+//  Created by MungYu-Lin on 16/5/19.
+//  Copyright © 2016年 MY. All rights reserved.
+//
+
+import XCTest
+@testable import Pull_to_Refresh_Table_View
+
+class Pull_to_Refresh_Table_ViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
